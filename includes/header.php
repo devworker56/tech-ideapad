734bf161cf195b7656632ca9f516bb1eba7ae1fd
devworker56 <alainkavena@gmail.com>
2025-10-23 10:52:34 -0400

    changed name of directory from image to images

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MDVA - Micro-Dons Vérifiables et Attribués</title>
     <!-- Favicon Setup -->
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="images/x-icon" href="/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
