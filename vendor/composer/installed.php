9032ff77c5dae7bc72644eed49c4618301facc19
devworker56 <alainkavena@gmail.com>
2025-11-16 18:59:08 -0500

    deleted and re-added vendor-pusher

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd13c0b300460439bbe560aeeb6a7aa42b5e84df8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd13c0b300460439bbe560aeeb6a7aa42b5e84df8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
